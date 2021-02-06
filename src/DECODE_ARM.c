4d4142109f76842de0e3d11af02c8386f9a3664f
Anderson Silva <andersonnsilva015@gmail.com>
2021-02-06 08:55:01 -0300

    novas mudanças

--- src/DECODE_ARM.c
+++ src/DECODE_ARM.c
@@ -1,11 +1,11 @@
 #include "DECODE_ARM.h"
-void printIntruction(FILE *f,  char* intruction){
+void printIntruction(FILE *f,  char* instruction){
 
-    f = fopen("code.txt","w");
+    f = fopen("code.txt","a");
 
     if(f == 0)
       printf("Não foi possivel escrever o arquivo!");
-    
-    fprintf(f,"%s\n", intruction);
+
+    fprintf(f,"%s\n", instruction);
     fclose(f);
 }
--- src/main.c
+++ src/main.c
@@ -4,24 +4,39 @@ int main(void)
 {
   FILE *f;
 
-  char intruction[20];    // Vetor que vai armazenar a tradução -> EX: mov r0, #3
+  char instruction[20];    // Vetor que vai armazenar a tradução -> EX: mov r0, #3
 
-  char aChar[4];
-  char* charPtr = aChar;
+  char string_in[4];
 
-  write(STDOUT_FILENO,"Digite uma sequência em Hexadecimal: ", 37);
+  printf("Digite uma sequencia\n");
+  scanf("%[^\n]", string_in);
 
-  read(STDIN_FILENO, charPtr, 1);  
-  while (*charPtr != '\n') {        
-    switch (*charPtr)
-    {
-    case '2':
-      printIntruction(f, "mov");
-      break;
-    default:
-      break;
+  switch (string_in[0]){
+  case '2':
+    strcat(instruction, "mov");
+    if(string_in[1] == '0'){
+      strcat(instruction, ", r0");
+    }else if(string_in[1] == '1'){
+      strcat(instruction, ", r1");
+    }else if(string_in[1] == '2'){
+      strcat(instruction, ", r2");
+    }else if(string_in[1] == '3'){
+      strcat(instruction, ", r3");
+    }else if(string_in[1] == '4'){
+      strcat(instruction, ", r4");
+    }else if(string_in[1] == '5'){
+      strcat(instruction, ", r5");
+    }else if(string_in[1] == '6'){
+      strcat(instruction, ", r6");
+    }else {
+      strcat(instruction, ", r7");
     }
-    read(STDIN_FILENO, charPtr, 1);
+    break;
+
+  default:
+    break;
   }
+
+  printIntruction(f, instruction);
   return 0;
 }
